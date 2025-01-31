a39eb2abccff22847a190ac57f25cdd84ec17bab
Oleksandr <dedikad@gmail.com>
2025-01-31 17:43:14 +0100

    make build

---
+++ index.27853d74.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=e.clientWidth,l=e.clientHeight,i=document.querySelector(".wall"),c=i.clientWidth,n=i.clientHeight;e.style.left=(c-t)/2+"px",e.style.top=(n-l)/2+"px";
+//# sourceMappingURL=index.27853d74.js.map
